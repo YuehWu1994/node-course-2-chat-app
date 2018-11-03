c4b37dfa6f99a632c4a4ee2bd5f274c64de6961b
Apple <rex9999772001@gmail.com>
2018-11-03 14:16:39 -0700

    Great new user and tell others

--- server/server.js
+++ server/server.js
@@ -16,19 +16,37 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+    //console.log('createMessage', newMessage);
     io.emit('newMessage', {
       from: newMessage.from,
       text: newMessage.text,
       createdAt: new Date().getTime()
     });
-  })
+
+    //broadcast: only received by other user
+    // socket.broadcast.emit("newMessage", {
+    //     from: newMessage.from,
+    //     text: newMessage.text,
+    //     createdAt: new Date().getTime()
+    // });
+  });
 
   socket.on('disconnect', () => {
     console.log('User was Disconnected');
-  })
+  });
 })
 
 server.listen(port, () => {
